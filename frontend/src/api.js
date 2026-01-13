5ead88284b7f6161358d008f4a2467a33838745f
GeetaBiradar63 <01fe23bcs282@kletech.ac.in>
2026-01-13 12:37:49 +0530

    Updated API baseURL for Render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-expense-tracker-backend-gwj1.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
